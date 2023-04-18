6a507c1ef33d6da7b5458df5740b481b37627abf
현석 이 <inc02179@naver.com>
2023-04-18 20:20:44 +0900

    docs : List.Add 주석

--- ListTest/List.cs
+++ ListTest/List.cs
@@ -6,20 +6,20 @@ using System.Threading.Tasks;
 
 namespace ListTest
 {
-    public class List<T>
+    public class List<T>        // List 클래스 선언
     {
         private int DefaultCapacity = 10;
 
         private T[] items;
         private int size;
 
-        public List()
+        public List()       // List 생성자 선언
         {
             this.items = new T[DefaultCapacity];
             this.size = 0;
         }
 
-        public void Add(T item)
+        public void Add(T item)     // Add 함수 구현
         {
             if (size < items.Length)
             {
@@ -27,12 +27,12 @@ namespace ListTest
             }
             else
             {
-                Grow();
+                Grow();     // size가 items.Length 보다 커졌을 경우 Grow함수를 호출해 새로운 배열을 생성 후 Add 진행
                 items[size++] = item;
             }
         }
 
-        public void Grow()
+        private void Grow()     // List에서 Add 도중 Capacity 범위를 벗어난 경우 새로운 배열 생성을 위한 함수
         {
             int newCap = items.Length * 2;
             T[] newItems = new T[newCap];
