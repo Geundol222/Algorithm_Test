8cdd09dd048c13ea3d68cfea33baed9334e9b3bb
현석 이 <inc02179@naver.com>
2023-04-18 20:17:15 +0900

    feat : List Add 구현
    List.Add 기능 구현

--- ListTest/List.cs
+++ ListTest/List.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ListTest
 {
-    internal class List
+    public class List<T>
     {
+        private int DefaultCapacity = 10;
+
+        private T[] items;
+        private int size;
+
+        public List()
+        {
+            this.items = new T[DefaultCapacity];
+            this.size = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (size < items.Length)
+            {
+                items[size++] = item;
+            }
+            else
+            {
+                Grow();
+                items[size++] = item;
+            }
+        }
+
+        public void Grow()
+        {
+            int newCap = items.Length * 2;
+            T[] newItems = new T[newCap];
+            Array.Copy(items, 0, newItems, 0, size);
+            items = newItems;
+        }
     }
 }
