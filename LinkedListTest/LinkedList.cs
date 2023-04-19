14f5f40c52be4fe67726e494659deba6b1beb33b
현석 이 <geundol222@gmail.com>
2023-04-19 20:20:25 +0900

    feat : LinkedListNode, LinkedList 생성자 및 변수 선언

--- LinkedListTest/LinkedList.cs
+++ LinkedListTest/LinkedList.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    internal class LinkedList
+    public class LinkedListNode<T>
     {
+        internal LinkedList<T> list;
+        internal LinkedListNode<T> prev;
+        internal LinkedListNode<T> next;
+        private T item;
+
+        public LinkedListNode(T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedList<T> List { get { return list; } }
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+        public T Value { get { return item; } }
+
+    }
+
+    public class LinkedList<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public LinkedListNode<T> First { get { return head; } }
+        public LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
     }
 }
