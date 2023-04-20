b7aacbc45626d252f7d4f263b17fbabbf6bcc8f2
현석 이 <geundol222@gmail.com>
2023-04-20 19:49:18 +0900

    feat : List IEnumerable 인터페이스 선언 및 구조체 생성
    반복기를 구현하기 위한 IEnumerable 인터페이스를 선언하고 반복기 구조체를 생성한다.

--- IteratorTest/LinkedList.cs
+++ IteratorTest/LinkedList.cs
@@ -4,9 +4,228 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IteratorTest
+namespace Iterator
 {
-    internal class LinkedList
+    public class LinkedListNode<T>
     {
+        internal LinkedList<T> list;
+        internal LinkedListNode<T> prev;
+        internal LinkedListNode<T> next;
+        private T item;
+
+        public LinkedListNode(T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedListNode(LinkedList<T> list, LinkedListNode<T> prev, LinkedListNode<T> next, T value)
+        {
+            this.prev = null;
+            this.next = null;
+            this.item = value;
+        }
+
+        public LinkedList<T> List { get { return list; } }
+        public LinkedListNode<T> Prev { get { return prev; } }
+        public LinkedListNode<T> Next { get { return next; } }
+        public T Value { get { return item; } }
+
+    }
+
+    public class LinkedList<T>
+    {
+        private LinkedListNode<T> head;
+        private LinkedListNode<T> tail;
+        private int count;
+
+        public LinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public LinkedListNode<T> First { get { return head; } }
+        public LinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+
+        public LinkedListNode<T> AddFirst(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddLast(T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            if (tail == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                newNode.prev = tail;
+                tail.next = newNode;
+                tail = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            if (node == null)
+                throw new ArgumentNullException();
+            if (node.list != this)
+                throw new InvalidOperationException();
+
+            if (node == tail)
+            {
+                newNode = AddLast(value);
+            }
+            else
+            {
+                newNode.next = node.next;
+                newNode.prev = node;
+                node.next.prev = newNode;
+                node.next = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(this, value);
+
+            if (node == null)
+                throw new ArgumentNullException();
+            if (node.list != this)
+                throw new InvalidOperationException();
+
+            if (node == head)
+            {
+                newNode = AddFirst(value);
+            }
+            else
+            {
+                newNode.prev = node.prev;
+                newNode.next = node;
+                node.prev.next = newNode;
+                node.prev = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public void Remove(LinkedListNode<T> node)
+        {
+            if (node == null)
+                throw new ArgumentNullException();
+            if (node.list != this)
+                throw new InvalidOperationException();
+
+            if (node == head)
+                head = node.next;
+            if (node == tail)
+                tail = node.prev;
+
+            if (node.prev != null)
+                node.prev.next = node.next;
+            if (node.next != null)
+                node.next.prev = node.prev;
+
+            count--;
+        }
+
+        public bool Remove(T value)
+        {
+            LinkedListNode<T> node = Find(value);
+            if (node != null)
+            {
+                Remove(node);
+                return true;
+            }
+            else return false;
+        }
+
+        public void RemoveFirst()
+        {
+            if (this == null)
+                throw new InvalidOperationException();
+
+            Remove(head);
+        }
+
+        public void RemoveLast()
+        {
+            if (this == null)
+                throw new InvalidOperationException();
+
+            Remove(tail);
+        }
+
+        public LinkedListNode<T>? Find(T value)
+        {
+            LinkedListNode<T> target = head; EqualityComparer<T> compare = EqualityComparer<T>.Default;
+            while (target != null)
+            {
+                if (compare.Equals(target.Value, value))
+                    return target;
+                else
+                    target = target.next;
+            }
+            return null;
+        }
+
+        public LinkedListNode<T>? FindLast(T value)
+        {
+            LinkedListNode<T> target = tail; EqualityComparer<T> compare = EqualityComparer<T>.Default;
+            while (target != null)
+            {
+                if (compare.Equals(target.Value, value))
+                    return target;
+                else
+                    target = target.prev;
+            }
+            return null;
+        }
+
+        public bool Contains(T value)
+        {
+            LinkedListNode<T> target = Find(value);
+            if (target != null) return true;
+            else return false;
+        }
     }
 }
