cf0ab4ec46ca4f461bc16b8915cd07f1d74c2f09
현석 이 <geundol222@gmail.com>
2023-04-20 20:49:42 +0900

    feat : List, LinkedList Sort, Average 함수 구현
    List와 LinkedList 어떠한 것이 들어와도 정렬과 평균을 구할 수 있는 Sort와 Average 기능 구현

--- IteratorTest/Program.cs
+++ IteratorTest/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Iterator.List<int> list = new Iterator.List<int>();
-            for (int i = 1; i <= 5; i++) list.Add(i);
+            for (int i = 5; i >= 1; i--) list.Add(i);
 
             foreach(int i in list)
                 Console.WriteLine(i);
@@ -13,10 +13,50 @@
             Console.WriteLine();
 
             Iterator.LinkedList<int> linkedList = new Iterator.LinkedList<int>();
-            for (int i = 1; i <= 5; i++) linkedList.AddLast(i);
+            for (int i = 5; i >= 1; i--) linkedList.AddLast(i);
 
-            foreach(int i in linkedList)
+            foreach (int i in linkedList)
                 Console.WriteLine(i);
+
+            Console.WriteLine();
+
+            Sort(list);
+            Sort(linkedList);
+
+            int listAvg = Average(list);
+            int linkedListAvg = Average(linkedList);
+        }
+
+        static void Sort<T>(IEnumerable<T> list) where T : IComparable
+        {
+            T[] array = list.ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length; j++)
+                {
+                    if (array[i].CompareTo(array[j]) > 0)
+                    {
+                        T temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static int Average(IEnumerable<int> list)
+        {
+            int sum = 0;
+            int avg = 0;
+            int[] array = list.ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum / array.Length;
         }
     }
 }
