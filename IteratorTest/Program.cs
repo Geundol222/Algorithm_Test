1c0416c06b9b22b2ac351e57544226972d5fb5cb
현석 이 <geundol222@gmail.com>
2023-04-21 02:05:37 +0900

    docs : Enumerator, Sort, Average 주석 추가
    Enumerator, Sort, Average 주석 추가

--- IteratorTest/Program.cs
+++ IteratorTest/Program.cs
@@ -33,8 +33,9 @@
             Console.WriteLine(arrAvg);
         }
 
-        static T[] Sort<T>(IEnumerable<T> list) where T : IComparable
+        static T[] Sort<T>(IEnumerable<T> list) where T : IComparable       // IEnumerable 인터페이스를 매개변수로 지정하고 IComparable을 제한자로 두어서 비교가 가능한 자료형만 해당 함수에 들어올 수 있게 하는 Sort함수 구현
         {
+            // 매개변수로 들어온 자료형을 array로 변환 후 Bubble Sort를 진행한다.
             T[] array = list.ToArray();
 
             for (int i = 0; i < array.Length; i++)
@@ -53,8 +54,9 @@
             return array;
         }
 
-        static double Average(IEnumerable<int> list)
+        static double Average(IEnumerable<int> list)    // int자료형을 가지는 IEnumerable 인터페이스로 하는 평균값 계산 함수
         {
+            // 매개변수로 들어온 자료형을 array로 변환 후 평균값을 계산한다.
             int sum = 0;
             double avg = 0;
             int[] array = list.ToArray();
