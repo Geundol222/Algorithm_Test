6f36b61b5184f3d09fe6917f0b5bc5b368b9a72c
현석 이 <geundol222@gmail.com>
2023-04-21 19:26:20 +0900

    feat : Stack 클래스 구현
    어댑터 패턴을 활용한 Stack 구현

---
+++ Stack & Queue Test/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class Stack<T>
+    {
+        private List<T> container;
+
+        public Stack()
+        {
+            container = new List<T>();
+        }
+
+        public void Push(T item)
+        {
+            container.Add(item);
+        }
+
+        public T Pop()
+        {
+            T item = container[container.Count - 1];
+            container.RemoveAt(container.Count - 1);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return container[container.Count - 1];
+        }
+    }
+}
