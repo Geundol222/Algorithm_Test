b8c3323f22760d8e8d9cd38fb3211ab084da9340
현석 이 <geundol222@gmail.com>
2023-04-22 17:17:17 +0900

    feat : 스택 계산기 구현
    스택을 이용하여 계산하는 기능, 사칙연산 우선순위 고려하여 계산 가능

--- Stack & Queue Test/Program.cs
+++ Stack & Queue Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text;
 
 namespace Stack___Queue_Test
 {
@@ -6,8 +7,12 @@ namespace Stack___Queue_Test
     {
         static void Main(string[] args)
         {
+            //string input = Console.ReadLine();
+            //BracketCheker(input);
+
             string input = Console.ReadLine();
-            BracketCheker(input);
+            double result = Calculator(input);
+            Console.WriteLine(result);
         }
 
         static void BracketCheker(string bracket)
@@ -20,7 +25,7 @@ namespace Stack___Queue_Test
 
             while (stack.Count > 0)
             {
-                char c = stack.Peek();
+                char c = stack.Pop();
 
                 foreach (char item in stack)
                 {
@@ -61,13 +66,132 @@ namespace Stack___Queue_Test
                                 break;
                             }
                     }
+                    break;
                 }
-                break;
             }
             if (stack.Count == 0)
                 Console.WriteLine("모든 괄호가 잘 맞습니다.");
             else
                 Console.WriteLine("괄호가 맞지 않습니다.");
+        }
+
+        static double Calculator(string input)
+        {
+            Stack<string> stack = new Stack<string>();
+            double op1 = 0;
+            double op2 = 0;
+
+            input = ChangeInput(input);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char item = input.ToArray()[i];
+
+                if (item != '+' && item != '-' && item != '*' && item != '/')
+                    stack.Push(item.ToString());
+                else
+                {
+                    op2 = double.Parse(stack.Pop().ToString());
+                    op1 = double.Parse(stack.Pop().ToString());
+
+                    switch (item)
+                    {
+                        case '+':
+                            stack.Push((op1 + op2).ToString());
+                            break;
+                        case '-':
+                            stack.Push((op1 - op2).ToString());
+                            break;
+                        case '*':
+                            stack.Push((op1 * op2).ToString());
+                            break;
+                        case '/':
+                            stack.Push((op1 / op2).ToString());
+                            break;
+                    }
+                }
+            }
+            return double.Parse(stack.Pop().ToString());
+        }
+
+        static string ChangeInput(string input)
+        {
+            Stack<char> opStack = new Stack<char>();
+            StringBuilder sb = new StringBuilder();
+
+            char[] calc = input.ToCharArray();
+
+            for (int i = 0; i < calc.Length; i++)
+            {
+                if (calc[i] != '+' && calc[i] != '-' && calc[i] != '*' && calc[i] != '/')
+                    sb.Append(calc[i]);
+                else if (opStack.Count == 0)
+                    opStack.Push(calc[i]);
+                else
+                {
+                    if (calc[i] == '(')
+                    {
+                        opStack.Push(calc[i]);
+                        continue;
+                    }
+                    else if (calc[i] == ')')
+                    {
+                        char c = ' ';
+                        while (true)
+                        {
+                            c = opStack.Pop();
+                            if (c == '(')
+                                break;
+                            else
+                                sb.Append(c);
+                        }
+                        continue;
+                    }
+
+                    if (OperatorFirst(opStack.Peek()) < OperatorFirst(calc[i]))
+                    {
+                        opStack.Push(calc[i]);
+                    }
+                    else
+                    {
+                        while (opStack.Count > 0)
+                        {
+                            if (OperatorFirst(opStack.Peek()) >= OperatorFirst(calc[i]))
+                                sb.Append(opStack.Pop());
+                            else
+                                break;
+                        }
+                        opStack.Push(calc[i]);
+                    }                    
+                }
+            }
+
+            char check = ' ';
+            while (opStack.Count > 0)
+            {
+                check = opStack.Pop();
+                if (check != '(')
+                    sb.Append(check);
+            }
+
+            return sb.ToString();
+        }
+
+        static int OperatorFirst(char c)
+        {
+            switch (c)
+            {
+                case '*':
+                case '/':
+                    return 3;
+                case '+':
+                case '-':
+                    return 2;
+                case '(':
+                    return 1;
+                default:
+                    return -1;
+            }
         }
     }
 }
