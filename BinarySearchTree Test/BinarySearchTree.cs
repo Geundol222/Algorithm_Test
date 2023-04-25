6f3ee14ee785b7cd044bc038e760f219d3c8299e
현석 이 <geundol222@gmail.com>
2023-04-25 20:19:05 +0900

    feat : 이진탐색트리 추가 구현
    이진 탐색트리에서 현재 노드보다 작으면 왼쪽 크면 오른쪽에 값을 추가하는 기능 구현

--- BinarySearchTree Test/BinarySearchTree.cs
+++ BinarySearchTree Test/BinarySearchTree.cs
@@ -6,7 +6,86 @@ using System.Threading.Tasks;
 
 namespace BinarySearchTree_Test
 {
-    internal class BinarySearchTree
+    public class BinarySearchTree<T> where T : IComparable<T>
     {
+        private Node root;
+
+        public BinarySearchTree()
+        {
+            this.root = null;
+        }
+
+        /// <summary>
+        /// 이진 탐색 트리에 값을 삽입하는 함수 Add
+        /// </summary>
+        /// <param name="item"></param>
+        public void Add(T item)
+        {
+            Node newNode = new Node(item, null, null, null);    // 매개변수로 받은 값을 가진 새로운 노드 생성
+
+            if (root == null)       // 만약 root가 비어있다면 새로운 노드를 root에 삽입
+                root = newNode;
+
+            Node current = root;
+            while (current != null)
+            {
+                // 만약 새로운 노드가 현재 노드보다 크다면 현재 노드의 오른쪽을 확인한 후, 오른쪽에 자식노드가 존재하면,
+                // 현재노드를 오른쪽 자식노드로 설정하고 다시 반복, 만약 오른쪽 자식노드가 존재하지않으면, 그 노드를 새로운 노드의 자리로 한다.
+                if (item.CompareTo(current.item) > 0)
+                {
+                    if (current.HasRightChild)
+                    {
+                        current = current.right;
+                    }
+                    else
+                    {
+                        current.right = newNode;
+                        newNode.parent = current;
+                        return;
+                    }
+                }
+                else if (item.CompareTo(current.item) < 0)
+                {
+                    // 만약 새로운 노드가 현재 노드보다 작다면 현재 노드의 왼쪽을 확인한 후, 왼쪽에 자식노드가 존재하면,
+                    // 현재노드를 왼쪽 자식노드로 설정하고 다시 반복, 만약 왼쪽 자식노드가 존재하지않으면, 그 노드를 새로운 노드의 자리로 한다.
+                    if (current.HasLeftChild)
+                    {
+                        current = current.left;
+                    }
+                    else
+                    {
+                        current.left = newNode;
+                        newNode.parent = current;
+                        return;
+                    }
+                }
+                else        // 만약 크지도 작지도 않다면 즉, 같은값이라면 중복된 갑이라는 뜻이므로 아무행동없이 함수를 종료한다.
+                    return;
+            }
+        }
+
+        public class Node
+        {
+            internal T item;
+            internal Node parent;
+            internal Node left;
+            internal Node right;
+
+            public Node(T item, Node parent, Node left, Node right)
+            {
+                this.item = item;
+                this.parent = parent;
+                this.left = left;
+                this.right = right;
+            }
+
+            public bool IsRootNode { get { return parent == null; } }
+            public bool IsLeftChildNode { get { return parent != null && parent.left == this; } }
+            public bool IsRightChildNode { get { return parent != null && parent.right == this; } }
+
+            public bool HasBothChild { get { return  left != null && right != null; } }
+            public bool HasLeftChild { get { return left != null && right == null; } }
+            public bool HasRightChild { get { return left == null && right != null; } }
+        }
     }
 }
