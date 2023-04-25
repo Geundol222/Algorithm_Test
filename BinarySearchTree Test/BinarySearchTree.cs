7d7d045afa5d9824fd89e4f41fd484c15e6c7a23
현석 이 <geundol222@gmail.com>
2023-04-25 21:04:35 +0900

    feat : 이진탐색트리 Remove 구현
    매개변수로 받은 item의 값을 가진 노드를 찾고 그 노드를 지우는 함수 EraseNode 함수를 이용한다.

--- BinarySearchTree Test/BinarySearchTree.cs
+++ BinarySearchTree Test/BinarySearchTree.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -61,6 +62,87 @@ namespace BinarySearchTree_Test
                 }
                 else        // 만약 크지도 작지도 않다면 즉, 같은값이라면 중복된 갑이라는 뜻이므로 아무행동없이 함수를 종료한다.
                     return;
+            }
+        }
+
+        /// <summary>
+        /// 매개변수로 받은 item의 값을 가진 노드를 찾고 그 노드를 지우는 함수 Remove
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool Remove(T item)
+        {
+            Node findNode = FindNode(item);
+
+            // FindNode 함수를 통해 찾고자하는 값을 가지는 노드를 확인한 후 노드가 존재한다면 그 노드를 지우고 true를 반환한다.
+            if (findNode != null)
+            {
+                EraseNode(findNode);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 찾은 노드를 지우기 위해 사용하는 함수 EraseNode
+        /// </summary>
+        /// <param name="node"></param>
+        private void EraseNode(Node node)
+        {
+            // 1. 찾은 노드에 자식이 없다면
+            if (node.HasNoChild)
+            {
+                // 노드가 왼쪽자식일 때는 노드 부모의 왼쪽 노드를 삭제하고 오른쪽 자식일때는 노드 부모의 오른쪽 노드를 삭제한다.
+                // 만약 노드가 root 노드라면 root를 삭제한다.
+                if (node.IsLeftChild)
+                    node.parent.left = null;
+                else if (node.IsRightChild)
+                    node.parent.right = null;
+                else
+                    root = null;
+            }
+            // 2. 찾은 노드에 자식이 하나만 존재한다면
+            else if (node.HasLeftChild || node.HasRightChild)
+            {
+                // 노드의 부모노드와 자식노드를 저장한다.
+                // 비교 연산자를 통해 노드의 자식노드가 왼쪽인지 오른쪽인지에 따라 다른 값을 저장한다.
+                Node parent = node.parent;
+                Node child = node.HasLeftChild ? node.left : node.right;
+
+                // 만약 노드가 부모의 왼쪽 자식노드라면 부모의 왼쪽을 노드의 자식 노드로 교체하고 자식노드의 부모를 노드의 부모노드로 교체한다.
+                if (node.IsLeftChild)
+                {
+                    parent.left = child;
+                    child.parent = parent;
+                }
+                // 만약 노드가 부모의 오른쪽 자식노드라면 부모의 오른쪽을 노드의 자식 노드로 교체하고 자식노드의 부모를 노드의 부모노드로 교체한다.
+                else if (node.IsRightChild)
+                {
+                    parent.right = child;
+                    child.parent = parent;
+                }
+                // 만약 노드가 root 노드라면 자식 노드를 root 노드로 바꾸고 자식 노드의 부모노드를 null로 한다.
+                else
+                {
+                    root = child;
+                    child.parent = null;
+                }
+            }
+            // 3. 찾은 노드에 자식이 둘 다 존재한다면
+            else
+            {
+                // 교체할 노드에 우선 현재 노드의 오른쪽 값을 저장한다.
+                Node removeNode = node.right;
+
+                // 노드의 오른쪽 값을 저장한 교체노드를 null이 아닐때까지 왼쪽으로 이동시킨 후 노드의 값과 교체노드의 값을 교체한다.
+                while (removeNode != null)
+                    removeNode = removeNode.left;
+
+                // EraseNode함수를 이용하여 교체할 노드를 지운다.
+                // 이 경우 자식노드가 없는 것으로 되어 1번 상황이 호출된다.
+                node.item = removeNode.item;
+                EraseNode(removeNode);
             }
         }
 
@@ -129,9 +211,10 @@ namespace BinarySearchTree_Test
             }
 
             public bool IsRootNode { get { return parent == null; } }
-            public bool IsLeftChildNode { get { return parent != null && parent.left == this; } }
-            public bool IsRightChildNode { get { return parent != null && parent.right == this; } }
+            public bool IsLeftChild { get { return parent != null && parent.left == this; } }
+            public bool IsRightChild { get { return parent != null && parent.right == this; } }
 
+            public bool HasNoChild { get { return left == null && right == null; } }
             public bool HasBothChild { get { return left != null && right != null; } }
             public bool HasLeftChild { get { return left != null && right == null; } }
             public bool HasRightChild { get { return left == null && right != null; } }
