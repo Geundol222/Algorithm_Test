adf53f07a95d6ee0884d8263bb80aa8afb56437c
현석 이 <geundol222@gmail.com>
2023-04-28 19:14:40 +0900

    docs : 정렬 알고리즘들의 원리

---
+++ 08. DesignTechnique Test/N and M.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._DesignTechnique_Test
+{
+    public class N_and_M
+    {
+        int num;
+        int count;
+        int[] array;
+        StringBuilder sb = new StringBuilder();
+
+        public void Run()
+        {
+            string str = Console.ReadLine();
+            string[] number = str.Split(' ');
+
+            num = int.Parse(number[0]);
+            count = int.Parse(number[1]);
+            array = new int[count];
+
+            
+        }
+    }
+}
