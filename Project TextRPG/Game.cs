70220f74449e2a700aa3946a2113d77e8b74004d
Geundol222 <geundol222@gmail.com>
2023-05-04 21:08:40 +0900

    feat : MainMenuScene

--- Project TextRPG/Game.cs
+++ Project TextRPG/Game.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Project_TextRPG
 {
-    internal class Game
+    public class Game
     {
+        private bool isRunning = true;
+        private Scene currentScene;
+        private MainMenuScene mainMenuScene;
+
+        public void Run()
+        {
+            Init();
+
+            while (isRunning)
+            {
+                Render();
+
+                Update();
+            }
+
+            Release();
+        }
+
+        public void Init()
+        {
+            mainMenuScene = new MainMenuScene(this);
+
+            currentScene = mainMenuScene;
+        }
+
+        public void Render()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            currentScene.Render();
+        }
+
+        public void Update()
+        {
+            currentScene.Update();
+        }
+
+        public void Release()
+        {
+
+        }
     }
 }
---
+++ Project TextRPG/Scene/MainMenuScene.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_TextRPG
+{
+    public class MainMenuScene : Scene
+    {
+        public MainMenuScene(Game game) : base(game)
+        {
+        }
+
+        public override void Render()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(" **********                   **     *******      *******       ******** ");
+            sb.AppendLine("/////**///                   /**    /**////**    /**////**     **//////**");
+            sb.AppendLine("    /**      *****  **   ** ******  /**   /**    /**   /**    **      // ");
+            sb.AppendLine("    /**     **///**//** ** ///**/   /*******     /*******    /**         ");
+            sb.AppendLine("    /**    /******* //***    /**    /**///**     /**////     /**    *****");
+            sb.AppendLine("    /**    /**////   **/**   /**    /**  //**  **/**       **//**  ////**");
+            sb.AppendLine("    /**    //****** ** //**  //**   /**   //**/**/**      /** //******** ");
+            sb.AppendLine("    //      ////// //   //    //    //     // // //       //   ////////  ");
+            sb.AppendLine();
+            sb.AppendLine();
+            sb.AppendLine("=========================================================================");
+            sb.AppendLine("                       게임을 시작하시겠습니까?                          ");
+            sb.AppendLine("1. 예");
+            sb.AppendLine("2. 아니오");
+            sb.AppendLine("=========================================================================");
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        public override void Update()
+        {
+            string input = Console.ReadLine();
+
+            int command;
+            if (!int.TryParse(input, out command))
+            {
+                Console.WriteLine("잘못 입력하셨습니다. 다시 입력해주세요");
+                Thread.Sleep(1000);
+                return;
+            }
+
+            switch (command)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+            }
+        }
+    }
+}
