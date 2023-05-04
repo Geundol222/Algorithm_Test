874bf57ae7343de681a6f13969f289b59c8a2b06
현석 이 <geundol222@gmail.com>
2023-05-05 02:10:02 +0900

    feat : CreatePlayer

--- Project TextRPG/Scene/CreatePlayerScene.cs
+++ Project TextRPG/Scene/CreatePlayerScene.cs
@@ -46,12 +46,34 @@ namespace Project_TextRPG
         public void CreateComplete()
         {
             StringBuilder sb = new StringBuilder();
+            int count = 4;
 
+            Console.Clear();
+            Console.WriteLine("캐릭터를 생성중입니다.");
+            while (count > 0)
+            {
+                Console.WriteLine(".");
+                Thread.Sleep(1500);
+                count--;
+            }
             Console.Clear();
             Console.WriteLine("캐릭터 생성이 완료되었습니다!");
             Thread.Sleep(1000);
             Console.Clear();
-            sb.AppendLine("캐릭터 생성이 완료되었습니다!")
+            sb.AppendLine($"당신{player.name}의 스탯은 다음과 같습니다.");
+            sb.AppendLine($"레벨        : {player.level}");
+            sb.AppendLine($"체력        : {player.hp}");
+            sb.AppendLine($"마력        : {player.mp}");
+            sb.AppendLine($"공격력      : {player.ap}");
+            sb.AppendLine($"방어력      : {player.dp}");
+            sb.AppendLine($"보유 골드   : {player.gold}");
+            sb.AppendLine($"경험치      : {player.exp}");
+            sb.AppendLine();
+            sb.AppendLine("능력치와 이름을 확정하시겠습니까?");
+            sb.AppendLine("1. 예");
+            sb.AppendLine("2. 아니오");
+
+            Console.WriteLine(sb.ToString());
         }
 
         private void CreateMent()
@@ -73,6 +95,26 @@ namespace Project_TextRPG
             player.dp = rand.Next(1, 5);
             player.gold = 0;
             player.level = 1;
+
+            CreateComplete();
+
+            string input = Console.ReadLine();
+
+            int command;
+            if (!int.TryParse(input, out command))
+            {
+                Console.WriteLine("잘못 입력하셨습니다. 다시 입력해주세요");
+                Thread.Sleep(1000);
+                return;
+            }
+
+            switch (command)
+            {
+                case 1:                    
+                    break;
+                case 2:
+                    return;
+            }
         }
     }
 }
